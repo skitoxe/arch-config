0b390f9c45d6e1b637896442f7c1c82df82c849e
skitoxe <heskel1@gmail.com>
2024-06-03 02:51:59 +0200

    fixar

--- arch_kickstarter.sh
+++ arch_kickstarter.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed git base-devel
+sudo pacman -Sy --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd -
 sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
 sudo sed -i '/color/s/^#//g' /etc/pacman.conf
-for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
-sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1)
+for word in $(cat yay-pkglist.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
+sudo pacman -Sy $(cat pacman-pkglist.txt | cut -d' ' -f1)
 sudo systemctl enable ly
-yay --devel --save
+#yay --devel --save
 sudo pacman -Syu
 sudo pacman -Qttdq | sudo pacman -Rns -
-sudo systemctl enable --now piavpn.service
+#sudo systemctl enable --now app.service
+sudo systemctl enable --now paccache.timer
 yay -Scc 
-sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
-chsh -s $(which zsh)
+#sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+#chsh -s $(which zsh)
 fc-cache -f -v
 ./cfg_dotfiles_kickstarter.sh
